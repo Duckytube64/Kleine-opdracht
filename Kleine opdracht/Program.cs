5ea3f3ea5d353903df13186e4e19cb74dc9ca163
Jeroen Hijzelendoorn <j.m.hijzelendoorn@students.uu.nl>
2019-11-19 21:44:44 +0100

    Add project files.

---
+++ Kleine opdracht/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SolverFoundation;
+using Microsoft.SolverFoundation.Common;
+using Microsoft.SolverFoundation.Services;
+
+namespace Kleine_opdracht
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
